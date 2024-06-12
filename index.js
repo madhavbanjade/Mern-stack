ca476b5db17c702686d0fd3fe88906a6a0b61e0c
madhavbanjade <madhavbanjade005@gmail.com>
2024-06-12 13:08:11 +0545

    learning javaScript

---
+++ index.js
@@ -0,0 +1,34 @@
+// data types 
+// number = 1,2,3
+// sting = 
+// boolen = 
+//operator => +,- , *,/
+
+
+
+/*
+console.log("Hello world");
+console.log("N-9 solution");
+console.log("Office");
+console.log("Chabahil");
+console.log(1+1);*/
+
+
+
+// console.log("a" + "b");
+// console.log("a" + "1");
+
+/*
+let name = 'madhav';
+let address = ' kathmandu';
+let age = 20;
+console.log(age, name, address) ;
+age = 30;
+console.log(age);*/
+
+let num1 = 1;
+let num2 =  2;
+console.log("a" - "a");
+console.log(`The  result of num1 and num2   = ${num1 + num2}`);
+
+
