764deaf566b1584f1bc57a5e905e020d0e4f4322
madhavbanjade <madhavbanjade005@gmail.com>
2024-06-13 12:53:27 +0545

    this is third commit

--- index.js
+++ index.js
@@ -30,8 +30,112 @@ console.log(age);*/
 // let num2 =  2;
 // console.log(`The  result of num1 and num2   = ${num1 + num2}`);
 
-console.log(javascripy);
+// console.log(javascripy);
 
-console.log(java)
+// console.log(java)
+/* 
+let a = "mad'hav";
+console.log(a);
+
+
+let b = 1+2+3+"4";
+console.log(b); */
+
+
+
+/*  we can not declare variable two times it throws => it throw error
+let add = 'ktm';
+let add = 'bhakt';
+ */
+
+/* console.log(1 + 2 "3" + 4 + 5); we can not add  string between the Number; */
+
+//string literals =>
+
+   /*  let num1 = 1;
+let num2 = 2;
+let sum =  `the sum of ${num1 } and ${num2}  = ${num1 + num2}`;
+console.log(sum); */
+
+
+/* 
+let name = "Madhav";
+let surName = "Banjade";
+let fullName = `My fullName is ${name} ${surName}`;
+console.log(fullName);
+ */
+
+//truthy and falsy value / boolen:=>
+    //single sting is falsy value other are truthy
+//0 is a falsy value and 1 is truthy
+
+/* let a = !!("false");
+let b = !!(" ");
+let c = !!("");
+let d =  !!(0);
+let e  =!!(1);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e); */
+//===, <, >, <=, >=
+
+
+
+
+// if-else
+
+/* let a = true;
+ if(!a) {
+console.log("i am if");
+ }else{ 
+console.log("i am else");
+ } */
+
+//  in if else condition is always block in always wrapped by paranthesis
+/* () => parantesis
+{} => curly braces or block
+[] =>  array */
+
+/* let name = 'madhav';
+if (name === 'hari'){
+    console.log("i am a");
+}else if(name  ===  'ram'){
+    console.log(" i am b");
+}else if(name === ' krishna'){
+    console.log("i am c");
+}else{
+    console.log("i am madhav");
+} */
+
+
+/*     let number = 10;
+    if (number > 11){
+        console.log("i am a")
+    }else if(number >= 10){
+        console.log("i am b")
+    }else{
+        console.log("i am c")
+    } */
+
+
+//if the value is true ! makes it false and vice-verse
+// !! makes or converts the value into boolen
+
+ let name = 'anish';
+if(!name ){
+    console.log('a');
+}else{
+    console.log('b');
+} 
+
+
+
+
+
+
+
+
 
 
